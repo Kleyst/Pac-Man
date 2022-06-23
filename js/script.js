d9051361b54e736f4a7c52b15a9503a503dc8669
Victor Bukhaltsev <victor.bukhaltsev@mail.ru>
2022-06-23 21:08:06 +0700

    v0.1 field drawing

--- js/script.js
+++ js/script.js
@@ -6,6 +6,71 @@
     colorW: "blue",   // wall color
     ghosts: ["red", "blue", "pink", "orange"],
     cherries: 3,
+    value: [
+      [ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 8, 0, 1, 2, 2, 3, 0, 1, 2, 2, 2, 3, 0, 7, 5, 0, 1, 2, 2, 2, 3, 0, 1, 2, 2, 3, 0, 4 ],
+      [ 8, 0, 7, 6, 6, 5, 0, 7, 6, 6, 6, 5, 0, 1, 1, 0, 7, 6, 6, 6, 5, 0, 7, 6, 6, 5, 0, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 8, 0, 1, 2, 2, 3, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 2, 2, 3, 0, 4 ],
+      [ 8, 0, 7, 6, 6, 5, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 7, 6, 6, 5, 0, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 7, 2, 2, 2, 2, 3, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 9, 9, 9, 9, 9, 4, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 9, 9, 9, 9, 9, 4, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 9, 9, 9, 9, 9, 4, 0, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 3, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 6, 6, 6, 6, 6, 5, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 8, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 8, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 4 ],
+      [ 8, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 4 ],
+      [ 8, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 4 ],
+      [ 8, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 4 ],
+      [ 8, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 8, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 4 ],
+      [ 8, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 4 ],
+      [ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 ],
+      [ 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5 ],
+    ],
+    drawField: function() {
+      let arrX0 = [0, 0, 0, 0, 0, 0, 0, 0];
+      let arrY0 = [0, 0, 0, 0, 1, 1, 1, 0];
+      let arrX1 = [0, 0, 1, 1, 1, 0, 0, 0];
+      let arrY1 = [0, 0, 0, 0, 0, 0, 0, 0];
+      let arrX2 = [1, 2, 2, 0, 2, 2, 2, 0];
+      let arrY2 = [2, 1, 1, 0, 1, 1, 1, 0];
+      let arrX3 = [2, 0, 1, 1, 1, 0, 1, 1];
+      let arrY3 = [1, 0, 2, 2, 2, 0, 1, 2];
+      for (let i = 0; i < this.value.length; ++i) {
+        for (let j = 0; j < this.value[i].length; ++j) {
+          if (this.value[i][j] != 0) {
+            ctx.beginPath()
+            ctx.fillStyle = "#332B99";
+            ctx.rect( 80 + arrX0[this.value[i][j] - 1] * 9 + j * 18,
+                      0  + arrY0[this.value[i][j] - 1] * 8 + i * 16,
+                      9   * arrX2[this.value[i][j] - 1],
+                      8   * arrY2[this.value[i][j] - 1]);
+            ctx.rect( 80 + arrX1[this.value[i][j] - 1] * 9 + j * 18,
+                      0  + arrY1[this.value[i][j] - 1] * 8 + i * 16,
+                      9   * arrX3[this.value[i][j] - 1],
+                      8   * arrY3[this.value[i][j] - 1]);
+            ctx.fill();
+          }
+          else {
+            ctx.beginPath()
+            ctx.fillStyle = "yellow";
+            ctx.rect(80 + j * 18, 0 + i * 16, 2, 2);
+            ctx.fill();
+          }
+        }
+      }
+    },
     info: {
       xGS: 30,
       yGS: 20,
@@ -57,12 +122,9 @@
       drawInfo: function() {
         this._splitDraw(this.textGS, this.size, " ", this.colorText, this.xGS, this.yGS, this.deltaY);
         this._splitDraw(this.value, this.size, "", this.colorValue, this.xGS, this.yGS + this.deltaB, this.deltaY);
-        this._splitDraw(this.textGS, this.size, " ", this.colorText, this.xHS, this.yHS, this.deltaY);
+        this._splitDraw(this.textHS, this.size, " ", this.colorText, this.xHS, this.yHS, this.deltaY);
         this._splitDraw(this.value, this.size, "", this.colorValue, this.xHS, this.yHS + this.deltaB, this.deltaY);
         this._drawLives(this.livesX, this.livesY, this.deltaL, this.size, this.livesColor, this.lives);
-      },
-      drawField: function() {
-        ;
       }
     },
   };
@@ -70,8 +132,9 @@
   const canvas = document.getElementById("canvas");
   canvas.width = field.w;
   canvas.height = field.h;
-  var ctx = canvas.getContext("2d");
+  let ctx = canvas.getContext("2d");
   ctx.fillStyle = field.colorB;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   field.info.drawInfo();
+  field.drawField();
 })();
